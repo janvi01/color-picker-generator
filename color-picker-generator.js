18442023202ad38a19999d90ec24434ae2323de3
janvi01 <janvibajo1@gmail.com>
2023-12-28 17:46:32 +0530

    feat: logic for colors in different formats

--- color-picker-generator.js
+++ color-picker-generator.js
@@ -1,14 +1,60 @@
-// color-picker-generator.js
+function generateRandomColor(format = "hex") {
+  const isValidFormat = ["hex", "rgb"].includes(format.toLowerCase());
 
-const getRandomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
+  if (!isValidFormat) {
+    throw new Error(
+      'Invalid color format. Supported formats are "hex" and "rgb".'
+    );
+  }
 
-const generateRandomColor = () =>
-  `#${(Math.random().toString(16) + "000000").slice(2, 8).toUpperCase()}`;
+  if (format.toLowerCase() === "hex") {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  } else {
+    return {
+      r: Math.floor(Math.random() * 256),
+      g: Math.floor(Math.random() * 256),
+      b: Math.floor(Math.random() * 256),
+    };
+  }
+}
 
-const generatePredefinedColor = (color) => color.toUpperCase();
+function generatePredefinedColor(colorName, format = "hex") {
+  const colorMap = {
+    red: "#FF0000",
+    green: "#00FF00",
+    blue: "#0000FF",
+    yellow: "#FFFF00",
+    purple: "#800080",
+    orange: "#FFA500",
+  };
+
+  const normalizedColorName = colorName.toLowerCase();
+  const isValidColor = Object.keys(colorMap).includes(normalizedColorName);
+
+  if (!isValidColor) {
+    throw new Error(
+      `Invalid color name. Supported color names are: ${Object.keys(
+        colorMap
+      ).join(", ")}.`
+    );
+  }
+
+  return format.toLowerCase() === "hex"
+    ? colorMap[normalizedColorName]
+    : hexToRgb(colorMap[normalizedColorName]);
+}
+
+function hexToRgb(hex) {
+  hex = hex.replace(/^#/, "");
+  const bigint = parseInt(hex, 16);
+  return {
+    r: (bigint >> 16) & 255,
+    g: (bigint >> 8) & 255,
+    b: bigint & 255,
+  };
+}
 
 module.exports = {
-  getRandomColor: generateRandomColor,
-  getPredefinedColor: generatePredefinedColor,
+  generateRandomColor,
+  generatePredefinedColor,
 };
